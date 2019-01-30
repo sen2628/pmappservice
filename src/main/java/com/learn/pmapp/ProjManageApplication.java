49b4cf118d2fdc117ff3ac6f9976eb3886be8b99
Senthilkumar Rajendran <sen2628@gmail.com>
2019-01-30 09:18:42 +0530

    changes

--- src/main/java/com/learn/pmapp/ProjManageApplication.java
+++ src/main/java/com/learn/pmapp/ProjManageApplication.java
@@ -2,9 +2,16 @@ package com.learn.pmapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjManageApplication {
+public class ProjManageApplication extends SpringBootServletInitializer {
+	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(ProjManageApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjManageApplication.class, args);
